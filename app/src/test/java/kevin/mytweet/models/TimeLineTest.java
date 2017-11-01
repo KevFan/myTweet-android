bec7fc123aa1b33d55f3b47c9d2fd174ac04e247
KevFan <kevin_fan@hotmail.co.uk>
2017-11-01 21:08:06 +0000

    Add test method stubs for timeline and user, + add tests for tweets

---
+++ app/src/test/java/kevin/mytweet/models/TimeLineTest.java
@@ -0,0 +1,33 @@
+package kevin.mytweet.models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kevin on 01/11/2017.
+ */
+public class TimeLineTest {
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void addTweet() throws Exception {
+  }
+
+  @Test
+  public void getTweet() throws Exception {
+  }
+
+  @Test
+  public void deleteTweet() throws Exception {
+  }
+
+}
---
+++ app/src/test/java/kevin/mytweet/models/UserTest.java
@@ -0,0 +1,24 @@
+package kevin.mytweet.models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kevin on 01/11/2017.
+ */
+public class UserTest {
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void testConstructor() {
+  }
+}
