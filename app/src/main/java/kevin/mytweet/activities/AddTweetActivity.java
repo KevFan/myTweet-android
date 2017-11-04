aa37f34cf4d9c77ce2062331ec6b3fefadf17e9f
KevFan <kevin_fan@hotmail.co.uk>
2017-11-04 22:27:15 +0000

    Add dialog box to confirm return to timeline for navigate up and on back press as tweet will not be saved on return from add tweet

--- app/src/main/java/kevin/mytweet/activities/AddTweetActivity.java
+++ app/src/main/java/kevin/mytweet/activities/AddTweetActivity.java
@@ -1,13 +1,22 @@
 package kevin.mytweet.activities;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
+import android.view.MenuItem;
+import android.view.View;
 
 import kevin.mytweet.R;
 import kevin.mytweet.fragments.AddTweetFragment;
 
+import static kevin.mytweet.helpers.IntentHelper.navigateUp;
 import static kevin.mytweet.helpers.LogHelpers.info;
+import static kevin.mytweet.helpers.LogHelpers.toastMessage;
 
 /**
  * Add Tweet activity
@@ -32,5 +41,54 @@ public class AddTweetActivity extends BaseActivity {
       fragment = new AddTweetFragment();
       manager.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
     }
+  }
+
+  /**
+   * Menu Item selector - only used for navigate up to previous activity here
+   * @param item Menu item
+   * @return Boolean
+   */
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    info("Base Activity - navigated up pressed");
+    final Activity activity = this;
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        confirmReturn( new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface arg0, int arg1) {
+            navigateUp(activity);
+          }
+        });
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  /**
+   * Override the on back pressed to display dialog to alert user that tweet will be discarded if
+   * they continue
+   * https://stackoverflow.com/questions/6413700/android-proper-way-to-use-onbackpressed-with-toast
+   */
+  @Override
+  public void onBackPressed() {
+    confirmReturn(new DialogInterface.OnClickListener() {
+      public void onClick(DialogInterface arg0, int arg1) {
+        AddTweetActivity.super.onBackPressed();
+      }
+    });
+  }
+
+  /**
+   * Helper for creating dialog box to reduce code duplication
+   * @param listener DialogInterface.OnClickListener listener - action on yes for dialog box
+   */
+  private void confirmReturn(DialogInterface.OnClickListener listener) {
+    new AlertDialog.Builder(this)
+        .setTitle("Tweet not saved !!")
+        .setMessage("Return to timeline and discard tweet?")
+        .setNegativeButton(android.R.string.no, null)
+        .setPositiveButton(android.R.string.yes, listener)
+        .setIcon(R.drawable.ic_error_outline_black_24dp)
+        .create().show();
   }
 }
